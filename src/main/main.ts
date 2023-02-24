6524b72db8023a9264c491537cbfc3057912f1c9
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-02-24 15:32:37 +0100

    fix autoupdate

--- src/main/main.ts
+++ src/main/main.ts
@@ -148,7 +148,7 @@ autoUpdater.on("update-downloaded", () => {
     type: "info",
     buttons: ["Restart", "Later"],
     title: "Application Update",
-    message: process.platform === "win32" ? releaseNotes : releaseName,
+    message: "No idea what to say here",
     detail: "A new version has been downloaded. Restart the application to apply the updates.",
   };
 
