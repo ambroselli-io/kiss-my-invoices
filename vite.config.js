28a8273b2177519d0170d011aef9a8076acba522
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-02-26 16:08:02 +0100

    deploy vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  root: "src/renderer",
+export default defineConfig(({ command, mode, ssrBuild }) => {
+  if (command === "serve") {
+    return {
+      plugins: [react()],
+      root: "src/renderer",
+    };
+  }
+  // command === 'build'
+  return {
+    plugins: [react()],
+    root: "src/renderer",
+    base: "/kiss-my-invoices",
+  };
 });
